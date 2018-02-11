e183752a96d6339a7757dc46fe2a30023e0ccf69
Grzegorz Kubiak <kingregor@gmail.com>
2018-02-11 12:35:00 +0100

    z mavenem

--- src/main/java/com/sda/javafx/Main.java
+++ src/main/java/com/sda/javafx/Main.java
@@ -10,10 +10,12 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/nowy_fx.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+
+
     }
 
     public static void main(String[] args) {
